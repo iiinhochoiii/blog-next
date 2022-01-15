d7d797317ec016975f648e91058de0913c704b62
dlsgh120 <dlsgh120@gmail.com>
2022-01-16 00:19:23 +0900

    빌드에러 수정2

--- stores/users/userStore.ts
+++ stores/users/userStore.ts
@@ -1,5 +1,5 @@
 import {action, observable, makeObservable} from 'mobx';
-import {checkIdStatus, loginStatus, userData} from './types';
+import {checkIdStatus, userData} from './types';
 import qs from 'qs';
 import client from '../../lib/client';
 
