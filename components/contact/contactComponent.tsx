c98924fb2b9a6db0ba0eafdc61ba858d72dca83e
dlsgh120 <dlsgh120@gmail.com>
2022-02-20 21:53:21 +0900

    build error 수정

--- components/contact/contactComponent.tsx
+++ components/contact/contactComponent.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import ContactActivity from './contactActivity';
 import { Box, Background, HeaderText, Text } from '@/components/Atom';
 
